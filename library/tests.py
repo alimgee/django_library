1ce780f60bfdc7c92d54f895123d38794ac7935c
alanmcgee <mcgee.al@gmail.com>
2022-06-04 16:33:13 +0100

    Adding basic model test

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from library.models import Author, Book
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_book_has_an_author(self):
+        book = Book.objects.create(title="The man in the high castle")
+        philip = Author.objects.create(first_name="Philip", last_name="K. Dick")
+        juliana = Author.objects.create(first_name="Juliana", last_name="Crain")
+        book.authors.set([philip.pk, juliana.pk])
+        #philip.book_set.add(book)
+        #juliana.book_set.add(book)
+        self.assertEqual(book.authors.count(), 2)
+        self.assertEqual(str(book), "The man in the high castle")
+        self.assertEqual(str(philip), "Philip K. Dick")
